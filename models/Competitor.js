c4201bb52a50a79d5bef123b74a45519c592fc79
am-neacsu <136963962+am-neacsu@users.noreply.github.com>
2025-07-25 17:32:39 +0100

    Update Competitor.js

--- models/Competitor.js
+++ models/Competitor.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const CompetitorSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  category: { type: String, required: true, enum: ['Under 2 years', 'Over 2 years'], default: 'Under 2 years' },
+  location: { type: String }
 });
 
 module.exports = mongoose.model('Competitor', CompetitorSchema);
